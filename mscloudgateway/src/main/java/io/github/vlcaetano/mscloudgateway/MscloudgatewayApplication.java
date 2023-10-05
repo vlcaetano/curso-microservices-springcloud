9267fe164beab3375a1d8530a1bf63339d4c8622
Vitor Caetano <vitorlc95@gmail.com>
2023-10-04 22:37:03 -0300

    mscloudgateway - configs iniciais

--- mscloudgateway/src/main/java/io/github/vlcaetano/mscloudgateway/MscloudgatewayApplication.java
+++ mscloudgateway/src/main/java/io/github/vlcaetano/mscloudgateway/MscloudgatewayApplication.java
@@ -2,12 +2,25 @@ package io.github.vlcaetano.mscloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class MscloudgatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MscloudgatewayApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/clientes/**").uri("lb://msclientes"))
+				.build();
+	}
 }
