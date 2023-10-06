8714ed4f476cf0e23cc87b7c6d52a37ce621f29b
Vitor Caetano <vitorlc95@gmail.com>
2023-10-06 19:54:11 -0300

    mscloudgateway - adicionando config mscartoes

--- mscloudgateway/src/main/java/io/github/vlcaetano/mscloudgateway/MscloudgatewayApplication.java
+++ mscloudgateway/src/main/java/io/github/vlcaetano/mscloudgateway/MscloudgatewayApplication.java
@@ -21,6 +21,7 @@ public class MscloudgatewayApplication {
 	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(r -> r.path("/clientes/**").uri("lb://msclientes"))
+				.route(r -> r.path("/cartoes/**").uri("lb://mscartoes"))
 				.build();
 	}
 }
